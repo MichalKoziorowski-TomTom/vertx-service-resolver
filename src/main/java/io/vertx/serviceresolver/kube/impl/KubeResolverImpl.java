68d2a114bd6ea1894e934d3a1c72f1abc1b32c9f
Michal Koziorowski <Michal.Koziorowski@tomtom.com>
2025-10-07 16:25:48 +0200

    Init

--- src/main/java/io/vertx/serviceresolver/kube/impl/KubeResolverImpl.java
+++ src/main/java/io/vertx/serviceresolver/kube/impl/KubeResolverImpl.java
@@ -12,21 +12,13 @@ package io.vertx.serviceresolver.kube.impl;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.*;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.Address;
 import io.vertx.core.net.SocketAddress;
 import io.vertx.core.spi.endpoint.EndpointBuilder;
 import io.vertx.core.spi.endpoint.EndpointResolver;
 import io.vertx.serviceresolver.ServiceAddress;
 import io.vertx.serviceresolver.kube.KubeResolverOptions;
-
-import java.util.List;
-import java.util.function.Function;
-
-import static io.vertx.core.http.HttpMethod.GET;
 
 public class KubeResolverImpl<B> implements EndpointResolver<ServiceAddress, SocketAddress, KubeServiceState<B>, B> {
 
@@ -39,10 +31,8 @@ public class KubeResolverImpl<B> implements EndpointResolver<ServiceAddress, Soc
   final String bearerToken;
 
   public KubeResolverImpl(Vertx vertx, KubeResolverOptions options) {
-
     HttpClientOptions httpClientOptions = options.getHttpClientOptions();
     WebSocketClientOptions wsClientOptions = options.getWebSocketClientOptions();
-
     this.vertx = vertx;
     this.wsClient = vertx.createWebSocketClient(wsClientOptions == null ? new WebSocketClientOptions() : wsClientOptions);
     this.httpClient = vertx.createHttpClient(httpClientOptions == null ? new HttpClientOptions() : httpClientOptions);
@@ -59,45 +49,7 @@ public class KubeResolverImpl<B> implements EndpointResolver<ServiceAddress, Soc
 
   @Override
   public Future<KubeServiceState<B>> resolve(ServiceAddress address, EndpointBuilder<B, SocketAddress> builder) {
-    return httpClient
-      .request(new RequestOptions()
-        .setMethod(GET)
-        .setServer(server)
-        .setURI("/api/v1/namespaces/" + namespace + "/endpoints"))
-      .compose(req -> {
-        if (bearerToken != null) {
-          req.putHeader(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken); // Todo concat that ?
-        }
-        return req.send().compose(resp -> {
-          if (resp.statusCode() == 200) {
-            return resp
-              .body()
-              .map(Buffer::toJsonObject);
-          } else {
-            return resp.body().transform(ar -> {
-              StringBuilder msg = new StringBuilder("Invalid status code " + resp.statusCode());
-              if (ar.succeeded()) {
-                msg.append(" : ").append(ar.result().toString());
-              }
-              return Future.failedFuture(msg.toString());
-            });
-          }
-        });
-      }).map(response -> {
-        String resourceVersion = response.getJsonObject("metadata").getString("resourceVersion");
-        KubeServiceState<B> state = new KubeServiceState<>(builder, this, vertx, resourceVersion, address.name());
-        JsonArray items = response.getJsonArray("items");
-        for (int i = 0;i < items.size();i++) {
-          JsonObject item = items.getJsonObject(i);
-          state.handleEndpoints(item);
-        }
-        return state;
-      }).andThen(ar -> {
-        if (ar.succeeded()) {
-          KubeServiceState<B> res = ar.result();
-          res.connectWebSocket();
-        }
-      });
+    return KubeServiceState.create(builder, this, vertx, address.name());
   }
 
   @Override
@@ -107,7 +59,6 @@ public class KubeResolverImpl<B> implements EndpointResolver<ServiceAddress, Soc
 
   @Override
   public void close() {
-
   }
 
   @Override
--- src/main/java/io/vertx/serviceresolver/kube/impl/KubeServiceState.java
+++ src/main/java/io/vertx/serviceresolver/kube/impl/KubeServiceState.java
@@ -10,10 +10,10 @@
  */
 package io.vertx.serviceresolver.kube.impl;
 
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpHeaders;
-import io.vertx.core.http.WebSocket;
-import io.vertx.core.http.WebSocketConnectOptions;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.*;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.SocketAddress;
@@ -27,14 +27,15 @@ class KubeServiceState<B> {
 
   final String name;
   final Vertx vertx;
-  final KubeResolverImpl resolver;
+  final KubeResolverImpl<B> resolver;
   final EndpointBuilder<B, SocketAddress> endpointsBuilder;
   String lastResourceVersion;
   boolean disposed;
   WebSocket ws;
   AtomicReference<B> endpoints = new AtomicReference<>();
+  private boolean refreshing;
 
-  KubeServiceState(EndpointBuilder<B, SocketAddress> endpointsBuilder, KubeResolverImpl resolver, Vertx vertx, String lastResourceVersion, String name) {
+  private KubeServiceState(EndpointBuilder<B, SocketAddress> endpointsBuilder, KubeResolverImpl<B> resolver, Vertx vertx, String lastResourceVersion, String name) {
     this.endpointsBuilder = endpointsBuilder;
     this.name = name;
     this.resolver = resolver;
@@ -42,13 +43,56 @@ class KubeServiceState<B> {
     this.lastResourceVersion = lastResourceVersion;
   }
 
+  static <B> Future<KubeServiceState<B>> create(EndpointBuilder<B, SocketAddress> builder, KubeResolverImpl<B> resolver, Vertx vertx, String serviceName) {
+    return listEndpoints(resolver)
+      .map(response -> {
+        String resourceVersion = response.getJsonObject("metadata").getString("resourceVersion");
+        KubeServiceState<B> state = new KubeServiceState<>(builder, resolver, vertx, resourceVersion, serviceName);
+        JsonArray items = response.getJsonArray("items");
+        if (items != null) {
+          for (int i = 0; i < items.size(); i++) {
+            JsonObject item = items.getJsonObject(i);
+            state.handleEndpoints(item);
+          }
+        }
+        return state;
+      })
+      .andThen(ar -> {
+        if (ar.succeeded()) {
+          // Initiate watch after initial state is prepared; do not wait for WebSocket to establish
+            ar.result().connectWebSocket();
+        }
+      });
+  }
+
+  private static Future<JsonObject> listEndpoints(KubeResolverImpl<?> resolver) {
+    return resolver.httpClient
+      .request(new RequestOptions()
+        .setMethod(HttpMethod.GET)
+        .setServer(resolver.server)
+        .setURI("/api/v1/namespaces/" + resolver.namespace + "/endpoints"))
+      .compose(req -> {
+        if (resolver.bearerToken != null) {
+          req.putHeader(HttpHeaders.AUTHORIZATION, "Bearer " + resolver.bearerToken);
+        }
+        return req.send().compose(resp -> {
+          if (resp.statusCode() == 200) {
+            return resp.body().map(Buffer::toJsonObject);
+          } else {
+            return resp.body().transform(ar -> {
+              StringBuilder msg = new StringBuilder("Invalid status code " + resp.statusCode());
+              if (ar.succeeded()) {
+                msg.append(" : ").append(ar.result());
+              }
+              return Future.failedFuture(msg.toString());
+            });
+          }
+        });
+      });
+  }
+
   void connectWebSocket() {
-    String requestURI = "/api/v1/namespaces/" + resolver.namespace + "/endpoints?"
-      + "watch=true"
-      + "&"
-      + "allowWatchBookmarks=true"
-      + "&"
-      + "resourceVersion=" + lastResourceVersion;
+    String requestURI = "/api/v1/namespaces/" + resolver.namespace + "/endpoints?watch=true&allowWatchBookmarks=true&resourceVersion=" + lastResourceVersion;
     WebSocketConnectOptions connectOptions = new WebSocketConnectOptions();
     connectOptions.setServer(resolver.server);
     connectOptions.setURI(requestURI);
@@ -56,15 +100,10 @@ class KubeServiceState<B> {
       connectOptions.putHeader(HttpHeaders.AUTHORIZATION, "Bearer " + resolver.bearerToken);
     }
     resolver.wsClient.webSocket()
-      .handler(buff -> {
-        JsonObject update  = buff.toJsonObject();
-        handleUpdate(update);
-      })
-      .closeHandler(v -> {
-        if (!disposed) {
-          connectWebSocket();
-        }
-      }).connect(connectOptions).onComplete(ar -> {
+      .handler(buff -> handleUpdate(buff.toJsonObject()))
+      .closeHandler(v -> { if (!disposed) connectWebSocket(); })
+      .connect(connectOptions)
+      .onComplete(ar -> {
         if (ar.succeeded()) {
           WebSocket ws = ar.result();
           if (disposed) {
@@ -72,13 +111,13 @@ class KubeServiceState<B> {
           } else {
             this.ws = ws;
           }
-        } else {
-          if (!disposed) {
-            // Retry WebSocket connect
-            vertx.setTimer(500, id -> {
-              connectWebSocket();
-            });
-          }
+        } else if (!disposed) {
+          String msg = ar.cause() != null ? ar.cause().getMessage() : null;
+            if (msg != null && msg.contains("410")) {
+              triggerRefreshFromList();
+              return;
+            }
+            vertx.setTimer(500, id -> connectWebSocket());
         }
       });
   }
@@ -86,33 +125,75 @@ class KubeServiceState<B> {
   void handleUpdate(JsonObject update) {
     String type = update.getString("type");
     JsonObject object = update.getJsonObject("object");
-    JsonObject metadata = object.getJsonObject("metadata");
-    String resourceVersion = metadata.getString("resourceVersion");
-    if (!lastResourceVersion.equals(resourceVersion)) {
-      handleEndpoints(object);
+    if (object == null) return;
+    if ("ERROR".equals(type)) {
+      Integer code = object.getInteger("code");
+      String reason = object.getString("reason");
+      if ((code != null && code == 410) || (reason != null && reason.toLowerCase().contains("expired"))) {
+        triggerRefreshFromList();
+        return;
+      }
     }
+    JsonObject metadata = object.getJsonObject("metadata");
+    if (metadata == null) return;
+    String resourceVersion = metadata.getString("resourceVersion");
+    if (resourceVersion != null && !resourceVersion.equals(lastResourceVersion)) {
+      handleEndpoints(object);
+      lastResourceVersion = resourceVersion;
+    }
+  }
+
+  private void triggerRefreshFromList() {
+    if (refreshing || disposed) return;
+    refreshing = true;
+    listEndpoints(resolver).onComplete(ar -> {
+      refreshing = false;
+      if (disposed) return;
+      if (ar.succeeded()) {
+        JsonObject response = ar.result();
+        String newRV = response.getJsonObject("metadata").getString("resourceVersion");
+        if (newRV != null) lastResourceVersion = newRV;
+        JsonArray items = response.getJsonArray("items");
+        if (items != null) {
+          for (int i = 0; i < items.size(); i++) {
+            JsonObject item = items.getJsonObject(i);
+            handleEndpoints(item);
+          }
+        }
+        if (ws != null) {
+          ws.close(); // close handler will reconnect with new RV
+        } else {
+          connectWebSocket();
+        }
+      } else {
+        vertx.setTimer(1000, id -> triggerRefreshFromList());
+      }
+    });
   }
 
   void handleEndpoints(JsonObject item) {
     JsonObject metadata = item.getJsonObject("metadata");
+    if (metadata == null) return;
     String name = metadata.getString("name");
     if (this.name.equals(name)) {
       JsonArray subsets = item.getJsonArray("subsets");
       EndpointBuilder<B, SocketAddress> builder = endpointsBuilder;
       if (subsets != null) {
-        for (int j = 0;j < subsets.size();j++) {
+        for (int j = 0; j < subsets.size(); j++) {
           List<String> podIps = new ArrayList<>();
           JsonObject subset = subsets.getJsonObject(j);
           JsonArray addresses = subset.getJsonArray("addresses");
           JsonArray ports = subset.getJsonArray("ports");
-          for (int k = 0;k < addresses.size();k++) {
+          if (addresses == null || ports == null) continue;
+          for (int k = 0; k < addresses.size(); k++) {
             JsonObject address = addresses.getJsonObject(k);
             String ip = address.getString("ip");
-            podIps.add(ip);
+            if (ip != null) podIps.add(ip);
           }
-          for (int k = 0;k < ports.size();k++) {
+          for (int k = 0; k < ports.size(); k++) {
             JsonObject port = ports.getJsonObject(k);
-            int podPort = port.getInteger("port");
+            Integer podPort = port.getInteger("port");
+            if (podPort == null) continue;
             for (String podIp : podIps) {
               SocketAddress podAddress = SocketAddress.inetSocketAddress(podPort, podIp);
               builder = builder.addServer(podAddress, podIp + "-" + podPort);
